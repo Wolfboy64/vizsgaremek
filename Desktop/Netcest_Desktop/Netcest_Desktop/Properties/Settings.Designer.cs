9122b96d2be7c35620cd5adf3865f5433585c18a
GellertK15 <kiraly.gellert@gmail.com>
2026-01-14 11:21:42 +0100

    v0.1.3.5

--- Desktop/Netcest_Desktop/Netcest_Desktop/Properties/Settings.Designer.cs
+++ Desktop/Netcest_Desktop/Netcest_Desktop/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Netcest_Desktop.Properties
+namespace CyberNest_Desktop.Properties
 {
 
 
